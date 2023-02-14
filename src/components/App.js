7c72a5ba084cd2718024240ff2454f3f47fb33dc
sunilkushwha786 <sunilkushwaha055@gmail.com>
2023-02-14 16:07:29 +0530

    rechange

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="Sunil"/>
+    <Welcome name=""/>
   )
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import React, {Component} from "react";
 const Welcome = (props) => {
     return (
         <div>
-            <h1>Hey !{props.name}</h1>
+            <h1>Hey{props.name}!</h1>
             <h2>Welcome to Newton School</h2>
         </div>
     )
